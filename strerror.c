41674d116ac60ce8f719522f7bc541afdae0277d
Reuben Hawkins <rhawkins@storix.com>
2014-11-04 15:47:34 -0800

    bootpd-2.4.3 from debian

---
+++ strerror.c
@@ -0,0 +1,21 @@
+/*
+ * strerror() - for those systems that don't have it yet.
+ * Doing it this way avoids yet another ifdef...
+ */
+
+/* These are part of the C library. (See perror.3) */
+extern char *sys_errlist[];
+extern int sys_nerr;
+
+static char errmsg[80];
+
+char *
+strerror(en)
+	int en;
+{
+	if ((0 <= en) && (en < sys_nerr))
+		return sys_errlist[en];
+
+	sprintf(errmsg, "Error %d", en);
+	return errmsg;
+}
